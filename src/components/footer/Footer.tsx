96239c8ceeb425caef20878305a2dc2c288cd382
nicod12 <dumenicolas45@gmail.com>
2024-08-22 19:59:40 -0300

    update

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,39 +6,39 @@ import { menuItems } from "../nav/NavBar";
 const Footer = () => {
   return (
     <footer>
-    <div className="grid grid-cols-1 lg:grid-cols-3 items-center w-full bottom-0 relative bg-[#51c2f1] text-white" >
-      <div className="flex justify-center pt-[4px] lg:my-3">
-        <img
-          src="../../assets/icons/logo-sa.png"
-          alt="Logo Supermercado Anlas"
-          width={150}
-          className="w-28 h-auto"
-        />
-      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-3 lg:py-5 items-center w-full bottom-0 relative bg-[#51c2f1] text-white" >
+        <div className="flex justify-center pt-[4px] lg:my-3">
+          <img
+            src="../../assets/icons/logo-sa.png"
+            alt="Logo Supermercado Anlas"
+            width={150}
+            className="w-28 h-auto"
+          />
+        </div>
 
-      <div id="menu__inf">
-        <nav>
-          <ul className="text-center text-[20px] lg:text-[18px] mb-[16px] opacity-80">
-          {menuItems.map((link, index) => (
-              <li key={index}>
-                <Link href={link.path}> <span>{link.name}</span>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </div>
+        <div id="menu__inf">
+          <nav>
+            <ul className="text-center text-[20px] lg:text-[18px] mb-[16px] opacity-80">
+              {menuItems.map((link, index) => (
+                <li key={index}>
+                  <Link href={link.path}> <span>{link.name}</span>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
 
-      <div>
-        <p className="text-center p-[1%] text-[16px] lg:text-[18px] mt-0 mx-0 mb-[16px] opacity-80">
-          Copyright © | 2024 | SUPERMERCADO ANLAS<br />
-          Todos los Derechos Reservados <br />
-          Diseño y Desarrollo por<br />
-          Andrea Guinder y Nicolás Dume
-        </p>
+        <div>
+          <p className="text-center p-[1%] text-[16px] lg:text-[18px] mt-0 mx-0 mb-[16px] opacity-80">
+            Copyright © | 2024 | SUPERMERCADO ANLAS<br />
+            Todos los Derechos Reservados <br />
+            Diseño y Desarrollo por<br />
+            Andrea Guinder y Nicolás Dume
+          </p>
+        </div>
       </div>
-    </div>
-  </footer>
+    </footer>
   )
 }
 
--- src/components/input/InputForm.tsx
+++ src/components/input/InputForm.tsx
@@ -12,18 +12,18 @@ interface Props {
 const InputForm = ({ name, control, label, type, error, required }: Props) => {
   return (
     <div className="mb-5">
-      <label htmlFor={name} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{label}</label>
+      <label htmlFor={name} className="block mb-2 text-sm lg:text-md , font-medium text-gray-900 dark:text-white">{label}</label>
       <Controller
         name={name}
         control={control}
         render={({ field }) =>
-          <input id={name} re type={type} required={required} {...field} className={`shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 ${error ? "border border-red-500 mt-1" : ""}`} />
+          <input id={name} re type={type} required={required} {...field} className={`shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm lg:text-md rounded-lg block w-full p-2.5 ${error ? "border border-red-500 mt-1" : ""}`} />
         }
       />
       {
         error && (
           <span className="bg-white mt-1.5 py-0.5 px-1 rounded-lg block">
-            <h4 className="text-red-700 text-sm font-bold ml-1">
+            <h4 className="text-red-700 text-sm lg:text-md font-bold ml-1">
               {error.message}
             </h4>
           </span>
