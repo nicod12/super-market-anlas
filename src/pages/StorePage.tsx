afa22095e9301d04cb16d93cfc7f2d4c099430ce
nicod12 <dumenicolas45@gmail.com>
2024-08-28 10:31:43 -0300

    update ui

--- src/pages/StorePage.tsx
+++ src/pages/StorePage.tsx
@@ -7,7 +7,7 @@ const StorePage: React.FC = () => {
 
   return (
     <section className='container mx-0'>
-      <h2 className="underline text-[#0e1c85] text-2xl text-center my-[3.5rem] ">Productos de nuestro negocio elegidos</h2>
+      <h2 className="underline text-[#0e1c85] text-2xl lg:text-3xl lg:text-start lg:ml-5 text-center my-[3.5rem] ">Productos de nuestro negocio elegidos</h2>
       <p className="text-center mb-4">El carrito está vacío.</p>
 
       <div className=" bg-[#06a2d8] p-1 opacity-50  my-7 mx-4 h-2"></div>
@@ -15,23 +15,24 @@ const StorePage: React.FC = () => {
       <div className=" bg-[#06a2d8] p-1 opacity-50 my-7 mx-4 h-2"></div>
       <div className=" bg-[#06a2d8] p-1 opacity-50 my-7 mx-4 h-2"></div>
 
-      <div>
-        <img src='/assets/images/MediosPago.webp' alt='image' />
-      </div>
+      <section className="grid grid-cols-1 lg:grid-cols-3">
+        <div>
+          <img src='/assets/images/MediosPago.webp' alt='image' className='w-auto h-auto lg:w-80 lg:h-[200px]' />
+        </div>
 
-      <div>
-        <img src='/assets/images/hero-asset.webp' alt='image' />
-      </div>
+        <div>
+          <img src='/assets/images/hero-asset.webp' alt='image' className='w-auto h-auto lg:w-80 lg:h-[200px]' />
+        </div>
 
-      <div>
-        <img src='/assets/images/mobile.webp' alt='image' />
-      </div>
-
+        <div>
+          <img src='/assets/images/mobile.webp' alt='image' className='w-auto h-auto lg:w-80 lg:h-[200px]' />
+        </div>
+      </section>
       <section>
         <p className="text-2xl text-[#0e1c85] font-bold mt-2.5 mx-auto mb-7 text-center">Dirección</p>
       </section>
 
-      <article className='flex flex-col'>
+      <article className='grid grid-cols-1 lg:grid-cols-4'>
         <div className="text-sm text-[#585656] bg-[#eeeeee]  border-2 border-[#585656] rounded-s w-[320px] my-5 p-5 mx-auto">Mitre 914</div>
         <div className="text-sm text-[#585656] bg-[#eeeeee]  border-2 border-[#585656] rounded-s w-[320px] my-5 p-5 mx-auto">Ciudad: Villa Rosa</div>
         <div className="text-sm text-[#585656] bg-[#eeeeee]  border-2 border-[#585656] rounded-s w-[320px] my-5 p-5 mx-auto">Provincia: Buenos Aires</div>
